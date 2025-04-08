0d6114a9c59b40799fefeb752608de5145f48d7c
Sharlin <sharlin.tang@gmail.com>
2025-04-08 10:35:06 +0800

    Update cypress.config.js with prpojectID and added .github\workflows\cypress.yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
+// npx cypress run --record --key c6cea708-0bee-4c1e-8599-3cae4d43cb1e
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "96srdf",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
